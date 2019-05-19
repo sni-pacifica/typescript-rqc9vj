5f07e2e2cc831b20fd64a60a1c7b69f519299800
sni-pacifica <sebastien.nichele@ca-pacifica.fr>
2019-05-19 14:40:25 +0200

    WIP on statistics on multiple simulation run.

--- Simulation.ts
+++ Simulation.ts
@@ -4,47 +4,85 @@ import { Predicates } from "./model";
 import { execTime } from "./Perfs";
 import { Logger } from "./Logger";
 
+export class Metrics {
+
+  constructor(readonly values: Array<number> = []) {
+    this.mean = values.reduce((memo, next) => memo + next, 0) / values.length;
+  }
+
+  addValue = (x: number) => new Metrics(this.values.concat(x));
+
+  private mean: number;
+  meanValue = () => {
+    return (this.mean)
+      ? this.mean
+      : this.mean = 5
+  }
+
+}
+
+type ExecutionTimes = Map<string, Metrics>;
+
 export class Simulation {
 
   constructor(readonly howMuchClientsToCreate: number) { }
 
-  run = () => {
-    const executionTimes: Map<string, number> = new Map();
+  run = (times: number = 1) => {
 
-    const [clients, creationTimeInMS] = execTime(() =>
-      Datagen.randomClients(this.howMuchClientsToCreate)
-    );
+    // totally arbitrary !
+    const warmupIteration = 3;
+    const executionTimes: ExecutionTimes = new Map();
 
-    executionTimes.set("creationTimeInMS", creationTimeInMS);
+    for (let i = 0; i <= warmupIteration + times; i++) {
 
-    const [filteredClients, filterOnePassTime] = execTime(() =>
-      Filtering.filterOnePass(
-        clients,
-        [
-          Predicates.nameContains("ef"),
-          Predicates.isAdult,
-          Predicates.isMale
-        ]
-      )
-    );
+      const [clients, creationTimeInMS] = execTime(() =>
+        Datagen.randomClients(this.howMuchClientsToCreate)
+      );
 
-    executionTimes.set("filterOnePassTime", filterOnePassTime);
-    
-    const [filteredClientsMulti, filterMultiplePass] = execTime(() =>
-      Filtering.filterMultiplePass(
-        clients,
-        [
-          Predicates.nameContains("ef"),
-          Predicates.isAdult,
-          Predicates.isMale
-        ]
-      )
-    );
+      if (i > warmupIteration) {
+        this.appendValueToExecutionTimeForKey(executionTimes, "creationTimeInMS", creationTimeInMS);
+      }
 
-    executionTimes.set("filterMultiplePass", filterMultiplePass);
+
+      const [filteredClients, filterOnePassTime] = execTime(() =>
+        Filtering.filterOnePass(
+          clients,
+          [
+            Predicates.nameContains("ef"),
+            Predicates.isAdult,
+            Predicates.isMale
+          ]
+        )
+      );
+      if (i > warmupIteration) {
+        this.appendValueToExecutionTimeForKey(executionTimes, "filterOnePassTime", filterOnePassTime);
+      }
+
+      const [filteredClientsMulti, filterMultiplePass] = execTime(() =>
+        Filtering.filterMultiplePass(
+          clients,
+          [
+            Predicates.nameContains("ef"),
+            Predicates.isAdult,
+            Predicates.isMale
+          ]
+        )
+      );
+
+      if (i > warmupIteration) {
+        this.appendValueToExecutionTimeForKey(executionTimes, "filterMultiplePass", filterMultiplePass);
+      }
+
+    }
 
     return executionTimes;
 
+  }
+
+  private appendValueToExecutionTimeForKey(m: ExecutionTimes, key: string, value: number) {
+    m.set(key,
+      (m.get(key) || new Metrics([])).addValue(value)
+    )
   }
 
 }
